96d4da0b51a47d3f8b5183a720c4d94f726cdef4
Med Hammouchi <143325013+0xG1iTch@users.noreply.github.com>
2026-02-05 09:33:41 +0100

    Add files via upload

---
+++ ex01/iter.hpp
@@ -0,0 +1,15 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template <typename A>
+void print(A element) {
+    std::cout << element << std::endl;
+}
+
+template <typename T, typename F>
+void iter(T* arr, const size_t length, F func)
+{
+    for(size_t i = 0; i < length; i++)
+        func(arr[i]);
+}
+#endif
